8509bdd4b0ed68c38361abf7ce4a98278a1cc964
Nicky Liu <nickyliu91@gmail.com>
2018-09-17 04:18:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,14 @@ class Application
     resp = Rack::Response.new
     resp.write "Hello, World"
     resp.finish
+
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+
+  resp.finish
   end
 
 end
